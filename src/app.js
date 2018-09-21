57079b27a94132ecd579c15a535c506a63c2d134
Jai Pandey <jai.pandey@sael.co>
2018-09-21 15:33:05 +0530

    attaching database configration

--- src/app.js
+++ src/app.js
@@ -5,24 +5,15 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './action/expenses';
-// import { setTextFilter} from './action/filter';
-// import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import './firebase/firebase';
 
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'water bill', amount: 400, createdAt: -1000 }));
 store.dispatch(addExpense({ description: 'Gas bill', amount: 2000, createdAt: 1000 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 1025000, createdAt: 200 }));
-// store.dispatch(setTextFilter("as"));
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'));
-// }, 3000);
-//const state = store.getState();
-//const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//console.log(visibleExpenses);
 
 const jsx =(
     <Provider store={store}>
@@ -66,3 +57,7 @@ ReactDOM.render(jsx, document.getElementById('app'));
 //for devlopment dependency which is not use as devlopment server use npm install chalk --dev
 //to install dependency of production server use npm install --production
 //use numeral js to modify currency format numeraljs.com  npm install numeral@2.0.6
+
+
+//database
+//install firebase database npm install firebase
